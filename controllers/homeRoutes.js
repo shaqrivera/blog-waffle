6ff082e5a9e6d1ca7e668b8df22eba4b52ea3e88
Shaquille Rivera <shaq.rivera@gmail.com>
2022-02-11 20:55:32 -0500

    Added routes and MVC structure

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,29 @@
+const router = require("express").Router();
+const { User } = require("../models");
+
+router.get("/", async (req, res) => {
+  try {
+    // Get all users, sorted by name
+    const userData = await User.findAll({
+      attributes: { exclude: ["password"] },
+      order: [["name", "ASC"]],
+    });
+
+    // Serialize user data so templates can read it
+    const users = userData.map((blogs) => blogs.get({ plain: true }));
+
+    // Pass serialized data into Handlebars.js template
+    res.render("homepage", { users });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+router.get("/login", async (req, res) => {
+  try {
+    res.render("login");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
---
+++ db/schema.sql
@@ -0,0 +1,4 @@
+DROP DATABASE IF EXISTS blog_db;
+CREATE DATABASE IF NOT EXISTS blog_db;
+
+USE blog_db;
